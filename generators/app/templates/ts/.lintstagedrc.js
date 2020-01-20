1344402fa2679b990335c16285d7e0a644f7266c
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2020-01-19 21:33:24 -0700

    Update deps

--- generators/app/templates/ts/.lintstagedrc.js
+++ generators/app/templates/ts/.lintstagedrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx,js,jsx}': ['eslint --fix', 'git add'],
-  '*.scss': ['stylelint --fix', 'git add']
+  '*.{ts,tsx,js,jsx}': ['eslint --fix'],
+  '*.scss': ['stylelint --fix']
 };
