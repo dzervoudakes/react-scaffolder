d926ff0b3759b6c1d9374f13c74e015b0f4595a6
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2020-08-02 12:09:37 -0600

    Update template dependencies, split typescript webpack config into a new package

--- generators/app/templates/typescript/build/webpack.dev.ts
+++ generators/app/templates/typescript/build/webpack.dev.ts
@@ -1,40 +1,9 @@
 /**
  * Webpack development configuration.
+ * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundlers/webpack-config-react-typescript/config/webpack.dev.js
+ * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundlers/webpack-config-react-typescript
  * @packageDocumentation
  */
-import path from 'path';
-import webpack from 'webpack';
-import FriendlyErrorsPlugin from 'friendly-errors-webpack-plugin';
-import HardSourcePlugin from 'hard-source-webpack-plugin';
-import WatchMissingNodeModulesPlugin from 'react-dev-utils/WatchMissingNodeModulesPlugin';
+const { webpackDevConfig } = require('@dztools/webpack-config-react-typescript');
 
-const PUBLIC_DIR = path.resolve(__dirname, '..', 'public');
-
-module.exports = {
-  mode: 'development',
-  devtool: 'cheap-module-eval-source-map',
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development'
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new FriendlyErrorsPlugin(),
-    new HardSourcePlugin(),
-    new WatchMissingNodeModulesPlugin(path.resolve(__dirname, '..', 'node_modules'))
-  ],
-  resolve: {
-    alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
-  },
-  optimization: {
-    noEmitOnErrors: true,
-    namedModules: true,
-    runtimeChunk: true
-  },
-  output: {
-    path: PUBLIC_DIR,
-    filename: path.join('js', '[name].js'),
-    chunkFilename: path.join('js', '[name].js')
-  }
-};
+module.exports = webpackDevConfig(__dirname);
--- generators/app/templates/typescript/build/webpack.prod.ts
+++ generators/app/templates/typescript/build/webpack.prod.ts
@@ -1,67 +1,9 @@
 /**
  * Webpack production configuration.
+ * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundlers/webpack-config-react-typescript/config/webpack.prod.js
+ * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundlers/webpack-config-react-typescript
  * @packageDocumentation
  */
-import path from 'path';
-import webpack from 'webpack';
-import { CleanWebpackPlugin as CleanPlugin } from 'clean-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
-import TerserPlugin from 'terser-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+const { webpackProdConfig } = require('@dztools/webpack-config-react-typescript');
 
-const BUILD_DIR = path.resolve(__dirname, '..', 'dist');
-
-const plugins = [
-  new webpack.EnvironmentPlugin({
-    NODE_ENV: 'production'
-  }),
-  new CleanPlugin(),
-  new MiniCssExtractPlugin({
-    filename: path.join('css', '[name].[chunkhash].min.css')
-  }),
-  new OptimizeCssAssetsPlugin(),
-  new webpack.optimize.AggressiveMergingPlugin()
-];
-
-if (process.argv.indexOf('--report') !== -1) {
-  plugins.push(new BundleAnalyzerPlugin());
-}
-
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  performance: {
-    hints: false
-  },
-  plugins,
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        extractComments: true,
-        parallel: true,
-        sourceMap: true
-      })
-    ],
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /node_modules/,
-          name: 'vendor',
-          chunks: 'all'
-        }
-      }
-    },
-    runtimeChunk: {
-      name: 'manifest'
-    }
-  },
-  output: {
-    path: BUILD_DIR,
-    publicPath: '/',
-    filename: path.join('js', '[name].[chunkhash].min.js'),
-    sourceMapFilename: path.join('js', '[name].[chunkhash].min.map'),
-    chunkFilename: path.join('js', '[name].[chunkhash].min.js')
-  }
-};
+module.exports = webpackProdConfig(__dirname);
