762503f18a8693cde770ecaabe0fdd2a157c32fc
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2020-10-08 19:29:21 -0600

    bundlers => bundling

--- generators/app/templates/javascript/build/webpack.dev.js
+++ generators/app/templates/javascript/build/webpack.dev.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Webpack development configuration.
- * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundlers/webpack-config-react/config/webpack.dev.js
- * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundlers/webpack-config-react#readme
+ * - Default config: https://github.com/dzervoudakes/dztools/blob/master/packages/bundling/webpack-config-react/config/webpack.dev.js
+ * - Notes on customizing/extending: https://github.com/dzervoudakes/dztools/tree/master/packages/bundling/webpack-config-react#readme
  */
 const { webpackDevConfig } = require('@dztools/webpack-config-react');
 
