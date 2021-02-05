76ac4834529b452b57a087abbdd0af3b657aa144
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2021-02-05 06:38:30 -0700

    Remove 'jest' from eslint extraneous deps list

--- generators/app/templates/javascript/.eslintrc.js
+++ generators/app/templates/javascript/.eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
         devDependencies: [
           'webpack.config.js',
           'build/**',
-          'jest/**',
           'scripts/**',
           '**/*.test.*',
           '**/*.spec.*'
