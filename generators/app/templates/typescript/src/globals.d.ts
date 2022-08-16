0c66ca6c6cc8f9f3e4fafc64d7850413e6986660
Dan Zervoudakes <6432641+dzervoudakes@users.noreply.github.com>
2022-08-16 11:02:08 -0600

    Remove duplicate module declaration

--- generators/app/templates/typescript/src/globals.d.ts
+++ generators/app/templates/typescript/src/globals.d.ts
@@ -78,8 +78,3 @@ declare module '*.module.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
-
-declare module '*.module.scss' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
